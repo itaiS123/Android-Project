118eb65485609f65537a448f2a4e1fa79ea76165
itaiS123 <itaishur@gmail.com>
2024-02-22 16:16:24 +0200

    added FirebaseInstance class in Reposetory

---
+++ REPOSITORY/src/main/java/il/co/shivhit/repository/FirebaseInstance.java
@@ -0,0 +1,32 @@
+package il.co.shivhit.repository;
+
+import android.content.Context;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+public class FirebaseInstance {
+    private static volatile FirebaseInstance _instance = null;
+    public static FirebaseApp app;
+    private FirebaseInstance(Context context) {
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setProjectId("")		// ApplicationId
+                .setApplicationId("")		// ProjectId
+                .setApiKey("")
+                .setStorageBucket("")
+                .build();
+
+        app = FirebaseApp.initializeApp(context, options);
+    }
+
+    public static FirebaseInstance instance(Context context) {
+        if (_instance == null) {  // 1st check
+            synchronized (FirebaseInstance.class) {
+                if (_instance == null){ // 2nd check
+                    _instance = new FirebaseInstance(context);
+                }
+            }
+        }
+        return _instance;
+    }
+}
